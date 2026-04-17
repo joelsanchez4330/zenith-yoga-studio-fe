9f2a1fe5a00d070ff867daf6105ac4488a5f73c3
joelsanchez4330 <joel.sanchez4330@gmail.com>
2026-04-17 16:48:24 +0700

    chore: inject AI generated components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+
 import configData from './data/site-config.json';
-import Hero_01 from './components/Hero/hero_01';
-import Feature_01 from './components/Features/feature_01';
+import Hero_03 from './components/Hero/hero_03';
+import Feature_02 from './components/Features/feature_02';
 import Gallery_01 from './components/Gallery/gallery_01';
 import Cta_01 from './components/CTA/cta_01';
 import Footer_01 from './components/Footer/footer_01';
@@ -11,8 +12,8 @@ export default function App() {
   
   return (
     <div className="w-full max-w-[1300px] mx-auto">
-      <Hero_01 config={configData} />
-      <Feature_01 config={configData} />
+      <Hero_03 config={configData} />
+      <Feature_02 config={configData} />
       <Gallery_01 config={configData} />
       <Cta_01 config={configData} />
       <Footer_01 config={configData} />
